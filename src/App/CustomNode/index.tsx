1e2bd5ca13ea3a06d8a017f5656011afc8d12258
maggie <waterlilypearl@gmail.com>
2025-12-03 23:41:32 -0500

    same as previous commit, forgot to add this file ooops

--- src/App/CustomNode/index.tsx
+++ src/App/CustomNode/index.tsx
@@ -1,18 +1,25 @@
 import { Handle, Position } from '@xyflow/react';
 import { useState } from 'react';
-// import type { NodeProps, Node } from '@xyflow/react';
 
 export type NodeData = {
   label: string;
+  status: string;
+  updateLabel: Function;
 }
 
 export function SelectorNode(props: any) {
   const data = (props.data ?? {}) as NodeData;
-  
+  const [ localLabel, setLocalLabel ] = useState(data.label);
+
   return (
     <>
       <label>Selector: </label>
-      <input defaultValue={data.label} placeholder="Type here..."/>
+      <input
+        value={localLabel}
+        placeholder="Type here..."
+        onChange={(evt) => setLocalLabel(evt.target.value)}
+        onBlur={data.updateLabel(localLabel)}
+      />
       <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} />
     </>
@@ -21,22 +28,30 @@ export function SelectorNode(props: any) {
 
 export function SequenceNode(props: any) {
   const data = (props.data ?? {}) as NodeData;
-  
+  const [ localLabel, setLocalLabel ] = useState(data.label);
+
   return (
     <>
       <label>Sequence: </label>
-      <input defaultValue={data.label} placeholder="Type here..."/>
+      <input
+        value={localLabel}
+        placeholder="Type here..."
+        onChange={(evt) => setLocalLabel(evt.target.value)}
+        onBlur={data.updateLabel(localLabel)}
+      />
       <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} />
     </>
   )
 }
 
-export const Checkbox = () => {
+export const Checkbox = (props: any) => {
   const [ isChecked, setIsChecked ] = useState(false);
+  const data = (props.data ?? {}) as NodeData;
 
   const checkHandler = () => {
     setIsChecked(!isChecked);
+    data.status = isChecked ? 'success' : 'failure';
   }
 
   return (
@@ -59,11 +74,17 @@ export const Checkbox = () => {
 
 export function ActionNode(props: any) {
   const data = (props.data ?? {}) as NodeData;
-  
+  const [ localLabel, setLocalLabel ] = useState(data.label);
+
   return (
     <>
       <label>Action: </label>
-      <input defaultValue={data.label} placeholder="Type here..."/>
+      <input
+        value={localLabel}
+        placeholder="Type here..."
+        onChange={(evt) => setLocalLabel(evt.target.value)}
+        onBlur={data.updateLabel(localLabel)}
+      />
       <Checkbox />
       <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} />
@@ -73,11 +94,16 @@ export function ActionNode(props: any) {
 
 export function ConditionNode(props: any) {
   const data = (props.data ?? {}) as NodeData;
-  
+  const [ localLabel, setLocalLabel ] = useState(data.label);
   return (
     <>
       <label>Condition: </label>
-      <input defaultValue={data.label} placeholder="Type here..."/>
+      <input
+        value={localLabel}
+        placeholder="Type here..."
+        onChange={(evt) => setLocalLabel(evt.target.value)}
+        onBlur={data.updateLabel(localLabel)}
+      />
       <Checkbox />
       <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} />
